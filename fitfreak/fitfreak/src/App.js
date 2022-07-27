b5f54b0971ad78bbc27a046645b9fed7a053d58b
cuia2 <adamcuiishere@gmail.com>
2022-07-27 12:22:39 -0400

    first commit

---
+++ fitfreak/fitfreak/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from './components/Navbar';
+
+export default function App() {
+  return (
+    <div><Navbar>Hello</Navbar></div>
+  )
+}
