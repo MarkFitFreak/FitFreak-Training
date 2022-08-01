d2bd944bf7f6abb6161f49a96eb34b850669ea23
brettanwar <banwar@emory.edu>
2022-08-01 08:43:18 -0700

    intro commit

--- fitfreak/fitfreak/src/App.js
+++ fitfreak/fitfreak/src/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './components/Navbar';
 import Octive from './components/Octive';
+import Intro from './components/Intro';
 
 export default function App() {
   return (
-    <div><Navbar></Navbar><Octive></Octive></div>
+    <div><Navbar></Navbar><Intro></Intro><Octive></Octive></div>
   )
 }
---
+++ fitfreak/fitfreak/src/components/Intro.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import background from './img/background.jpg';
+console.log(background)
+
+
+export default function Intro() {
+  return (
+    <div>Intro
+        <img src = {background}/>
+    </div>
+    
+  )
+  }
+
+  console.log(background)
+
+
+
