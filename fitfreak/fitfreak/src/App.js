fb7e18edd0289d58bd1bedf259334df07c6e6cd9
cuia2 <adamcuiishere@gmail.com>
2022-08-04 17:40:33 -0400

    Trainers commit

--- fitfreak/fitfreak/src/App.js
+++ fitfreak/fitfreak/src/App.js
@@ -3,11 +3,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './components/Navbar';
 import Octive from './components/Octive';
 import TrainerPage from './components/TrainerPage';
+import Intro from './components/Intro';
 
 
 export default function App() {
   return (
-    <div><Navbar></Navbar><Octive></Octive> <TrainerPage></TrainerPage></div>
+    <div><Navbar></Navbar><Intro></Intro><Octive></Octive><TrainerPage></TrainerPage></div>
    
   )
 }
