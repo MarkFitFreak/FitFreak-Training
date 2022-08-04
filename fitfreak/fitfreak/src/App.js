483afeabe89df5c8770d5bf2de8318989cd7b297
Juan Valderrama <jvalderr@usc.edu>
2022-08-04 17:22:09 -0400

    TrainerPage Juan

--- fitfreak/fitfreak/src/App.js
+++ fitfreak/fitfreak/src/App.js
@@ -2,9 +2,12 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './components/Navbar';
 import Octive from './components/Octive';
+import TrainerPage from './components/TrainerPage';
+
 
 export default function App() {
   return (
-    <div><Navbar></Navbar><Octive></Octive></div>
+    <div><Navbar></Navbar><Octive></Octive> <TrainerPage></TrainerPage></div>
+   
   )
 }
