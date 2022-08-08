4af8a1dbd7e37afd91ef12d1154cedd69d1d3d6d
Johnrev16 <jbermudez@middlebury.edu>
2022-08-08 11:29:39 -0400

    intro-john

--- fitfreak/fitfreak/src/components/Intro.js
+++ fitfreak/fitfreak/src/components/Intro.js
@@ -7,62 +7,92 @@ import './Intro.css';
 export default function Intro() {
   return (
     <div>
-      <div className = "imgs">
+      <div className = "container center-fit">
         <img className="center-fit" src = {team}/>
-        <div className="centered">FITFREAK</div>
-        <div className="centered">MOVE. IMPROVE. REPEAT.</div>
-        <img className="center-fit" src = {background}/>
+        <div className = "centered">
+          <div className = "text-center imgs">
+            <b className = "fontUp dark">FITFREAK</b>
+            <br></br>
+            <b className = "fontUp1 dark">MOVE. IMPROVE. REPEAT.</b>
+          </div>
+
+          <div className="dorian">
+            <b>Dorian</b>
+          </div>
+
+          <div className="miguel">
+            <b>Miguel</b>
+          </div>
+
+          <div className="andile">
+            <b>Andile</b>
+          </div>
+
+          <div className="jarrod">
+            <b>Jarrod</b>
+          </div>
+
+          <div className = "chad">
+            <b>Chad</b>
+          </div>
+
+          <div className = "stephen">
+            <b>Stephen</b>
+          </div>
+
+          <div className="mark1">
+            <b>Mark</b>
+          </div>
+
+        </div>
       </div>
-        <p>
-        FITFREAK (use logo)
+      <div>
+          <img className="center-fit" src = {background}/>
+          <div className = "text-center imgs">
+            <b></b>
+            <br></br>
+            <b></b>
+          </div>
+      </div>
+      <p>
+        We are the more than just a place to break sweat – we’re an incubator for achieving the best version of yourself. 
 
-MOVE. IMPROVE. REPEAT.
+        This is the basis of the FitFreak philosophy.
+        
+        Whether you’re a novice or a competing athlete, a well-rounded training program is essential to your overall wellbeing. 
 
-At FitFreak we train smart. 
+        Movement and technique form the basis of our fitness regimens, with rehabilitation and injury prevention our specialised fundamental focus areas. 
 
-We are the more than just a place to break sweat – we’re an incubator for achieving the best version of yourself. 
+        Through benchmarked movement assessments, we coach you to strengthen your body movement patterns, prevent injury, and educate you to become more self-aware. 
 
-This is the basis of the FitFreak philosophy. 
+        The success of this unconventional training approach is proven by the results our clientele boast. Not only are they surpassing their personal fitness goals— be it weight loss, or increased performance — they are doing it in a safe environment under the FitFreak team’s eagle eyes. 
 
-------------
+        ------------
+        Accountability. Community. Prehab. Strength Training. Positive Energy. 
 
-Whether you’re a novice or a competing athlete, a well-rounded training program is essential to your overall wellbeing. 
-
-Movement and technique form the basis of our fitness regimens, with rehabilitation and injury prevention our specialised fundamental focus areas. 
-
-Through benchmarked movement assessments, we coach you to strengthen your body movement patterns, prevent injury, and educate you to become more self-aware. 
-
-The success of this unconventional training approach is proven by the results our clientele boast. Not only are they surpassing their personal fitness goals— be it weight loss, or increased performance — they are doing it in a safe environment under the FitFreak team’s eagle eyes. 
-
-------------
-
-
-Accountability. Community. Prehab. Strength Training. Positive Energy. 
-
-These are the by 5 pillars by which we are driven — and the same pillars we use to guide you on your fitness and wellness journey. 
+        These are the by 5 pillars by which we are driven — and the same pillars we use to guide you on your fitness and wellness journey. 
 
 
-Personal Training:
-A great way to start your journey. With a designated PT by your side, you’re guaranteed an even closer alignment with your personal goals.
-We offer sessions focused on, but not limited to: 
-•	Rehab: making sure you come back stronger post injury, or
-•	Prehab: ensuring that your body is at optimal performance to support you.
+        Personal Training:
+        A great way to start your journey. With a designated PT by your side, you’re guaranteed an even closer alignment with your personal goals.
+        We offer sessions focused on, but not limited to: 
+        •	Rehab: making sure you come back stronger post injury, or
+        •	Prehab: ensuring that your body is at optimal performance to support you.
 
-Group Training:
-We work as a team to create regimens that are safe, dynamic, and ultimately remain challenging. 
+        Group Training:
+        We work as a team to create regimens that are safe, dynamic, and ultimately remain challenging. 
 
-Our team will be there to support you during these sessions.
+        Our team will be there to support you during these sessions.
 
-Run Crew:
-This is a social affair. We come together to run together, safely. 
+        Run Crew:
+        This is a social affair. We come together to run together, safely. 
 
-Begin your running journey or improve your running performance by joining this group of like-minded individuals. 
+        Begin your running journey or improve your running performance by joining this group of like-minded individuals. 
 
-*Join the FitFreak tribe* 
+        *Join the FitFreak tribe* 
 
-Our trainers are friendly and lead by example. They are paramount in building the FitFreak community. Through their expert support, you’ll exceed your own expectations and achieve success beyond what you ever thought possible. 
-
-        </p>
+        Our trainers are friendly and lead by example. They are paramount in building the FitFreak community. Through their expert support, you’ll exceed your own expectations and achieve success beyond what you ever thought possible. 
+      </p>
     </div>
   )
   }
