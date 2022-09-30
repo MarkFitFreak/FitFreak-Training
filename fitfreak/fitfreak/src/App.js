752b6a032e272d6f000a78619b6a872dedf72e05
swyx <michelle.shelfish@gmail.com>
2022-09-30 09:49:17 +0200

    brett merge

--- fitfreak/fitfreak/src/App.js
+++ fitfreak/fitfreak/src/App.js
@@ -3,9 +3,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './components/Navbar';
 import Octive from './components/Octive';
 import Intro from './components/Intro';
+import TrainerPage from './components/TrainerPage';
 
 export default function App() {
   return (
-    <div><Navbar></Navbar><Intro></Intro><Octive></Octive></div>
-  )
-}
+    <><div><Navbar></Navbar><Intro></Intro><Octive></Octive></div><div><Navbar></Navbar><Octive></Octive> <TrainerPage></TrainerPage></div></>
+  )}
---
+++ fitfreak/fitfreak/src/components/TrainerPage.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './TrainerPage.css'; 
+export default function TrainerPage() {
+    return (
+        <div className='container-fluid card bg-secondary'>
+            <h1 className='text-center display-1 text-white'>Our Team</h1>
+            <div className='card-body bg-secondary'>
+                <div className='row'>
+                <div className='col-sm-4 wrap'>
+                    <img class="card-img-top" src="https://previews-te.wetransfer.net/file/wetransfer/j1oc/sgsxddn0a3y7tg6mh20220729115838/s0k3v7ehsq1fgwwd220220729115838?width=2048&height=2048&source=spaceship&origin=collect-free&s=12ceb65766cacaac687d11ad55aa82dd5a70b516&Expires=1659536081&Signature=PEXfxTmO2IN5hltzpg4zWYuvRBUU~AGdot4NxZwjvVAh3MEB8Jmk2nrA1xE5KF99ZRxcNquo3SZ0Ryphc6yV-3V7bZUecjwFN~Uk6ETmEdeNpZjS4JjemWW4K5DRiYOGHIXEUr3z9ez1gEaRwR8z7AXiCMFeSAF~d0EpkETA8iD8DtRYEAdP94hkYF5fVAadRt2sSw7rASx7skXKMTdYbGy-FBNb~TAeo4qt0Qp1YUUyVhwPaT9CtlmMAfFK3nwPwPVpzb3DMSguJ7O~sjnMZyGR8Zl6D6PvESPYVjjMPF-2XhPlFEYRUsvTyqYgv~fJ1ti7pQLVHE7tw8t3a8NiwQ__&Key-Pair-Id=APKAIRLQFERKGUWFG7GQ" alt="Card image cap"/>
+                    <div className='text display-6 text-center'>Mark<br></br><br></br>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</div>
+                </div>
+                <div className='col-sm-4 wrap'>
+                    <img class="card-img-top" src="https://previews-te.wetransfer.net/file/wetransfer/j1oc/sgsxddn0a3y7tg6mh20220729115838/s9vrhxwj147qeixjy20220729115838?width=2048&height=2048&source=spaceship&origin=collect-free&s=88cc00189fb77cb8c7f32e8ce912926aa35d0835&Expires=1659541496&Signature=VoFNh4vBuLgcmfeHRNl8WeFwS72-QmEhgkQyRkwIblT597LaISqdzG0rIPI4bPZuoBW9Kla1HYLp356gp7X2glOOHgNh4Dn-O5FIQRheBYcdZ-EgBkpVe7BtwsMmuk4tqzcVU~tamVjbvOOF5SvzgIpPVOmskIc7cgpo-Hs0g0K5tBSGU5W2vKyS-bd5-CTS-3WPdIDJG2IMf8GwI3~~UgiCMkM8N36ULboh-XQ5McYC8PVa-Bx4ciMb-rxX3fdTjZg38bdRFMu5iGSxnifIXAWHTiMGJ-W1upK0lYHRsFhpyZimCDyiDiAcW3VWsmVSC-yByp7GTFAPfVn3UiLRXw__&Key-Pair-Id=APKAIRLQFERKGUWFG7GQ" alt="Card image cap"/>
+                    <div className='text display-6 text-center'>Zak<br></br><br></br>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</div>
+                </div>
+                <div className='col-sm-4 wrap'>
+                    <img class="card-img-top" src="https://previews-te.wetransfer.net/file/wetransfer/j1oc/sgsxddn0a3y7tg6mh20220729115838/sn7njwsrqgkf2j50120220729115838?width=512&height=512&source=spaceship&origin=collect-free&s=7826ac28af49553c4707fa9e92585b581be9ec51&Expires=1659541484&Signature=JzqIUwMiHQJ9sgOe-BB0OjKWOnbTBzLTKBhNeiTHt7r0RNb0A5FY3J-yOEXHSy~Yyc1CeSXQ7RbwdguuFywZwvCEWHK3Tkto329LjLhz-SUN1vZiFdRz90mKa~EH9leAWU0wNDUoO~eqR01toquuVucQJZOz3gYEip8Opa8LFNsDko2TCugIko9q3ecrUs9eUiAAtcBRjlykY2xwfYIr4kP2956v-ZCctvDky0OmaNGwtoO7te~-FNrzxAos2mwH1HZ6H2TetuLX3sMwEoOVzJWXbiPxiZhChpn16oMOp4TutqA5GJABuWcNGWvIOH4XDkww-TDdu5z7GTwvDnzvyg__&Key-Pair-Id=APKAIRLQFERKGUWFG7GQ" alt="Card image cap"/>
+                    <div className='text display-6 text-center'>Jay<br></br><br></br>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</div>
+                </div>
+                </div>
+
+                <div className='row mt-3'>
+                    <div className='col-sm-4 wrap'>
+                    <img class="card-img-top" src="https://previews-te.wetransfer.net/file/wetransfer/j1oc/sgsxddn0a3y7tg6mh20220729115838/scyfvrb78sa02hv6320220729115838?width=512&height=512&source=spaceship&origin=collect-free&s=8eba3f79efffacee5082a6ee581b85045afce9f2&Expires=1659541484&Signature=d9EHgHgW2xqgKWgrUw-2klYA2C7GNOD43sUaVF3l8GDRzbM2VkniLvYQ0gyF0zjbbmIsJbmwcn~308DD4l9z3qk-G6otX49oXbCoq40VCkgKJqESxrqAB-bAtbF7JfGEgornTzVFADW7n6VqOUHEXiQnGcU4fgZ1H72budEe4yIOy74P8UExApPwrsrZf5NX~gUFFkGpMBbsmp9FFWyDS9kDR6jdHZ5YvyvySqz5xZm9~8bAaDAC~CgbQ8RuTf2atR4xifLPHtulSQE4kQgtl9fQPY40BbL2kNMyME3PAwU6Frk3VVIXkqqAfJJ2IS73P-tQiWdEU-Lu~fJ~JTmlCg__&Key-Pair-Id=APKAIRLQFERKGUWFG7GQ" alt="Card image cap"/>
+                <div className='text display-6 text-center'>Jeff<br></br><br></br>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</div>
+                    </div>
+                <div className='col-sm-4 wrap'>
+                    <img class="card-img-top" src="https://previews-te.wetransfer.net/file/wetransfer/j1oc/sgsxddn0a3y7tg6mh20220729115838/sq5asiw3syrnp7p6x20220729115838?width=512&height=512&source=spaceship&origin=collect-free&s=83d7568a82fdf0fc14805a8673a40d7088405c30&Expires=1659541484&Signature=cqpPah00vtHkxmyBXtrKjEYjAyijfExqOsk7tZD8G9SkYoJN9UZWDugVWz-2p3F9m3sHNL0RdYY9HB1J67BkUawzSx3Vpx8QN0OH1ZgG6X1NfPZh5xcOK81YfqHWQ5Yh4-0d-J0xx7~Yoox7RRmwz4VBkWQbIRaisjamGsYJw~Sz1~HjiteWwuGIDEnrHSiw7eD73pEBIH5nRy6fVS9~clv~y5RNPCISHM7gqke3dpU3KnsE0eyRfj3mQUGQIIVKEEPW19QweRRYaHGud5fY9I3JJgh2BVAVqSIjcd4Pt26FTZnCKrQvmCzdiVda8hyVFrE5ueErvdFJQjTRfwV8rg__&Key-Pair-Id=APKAIRLQFERKGUWFG7GQ" alt="Card image cap"/>
+                    <div className='text display-6 text-center'>Ken<br></br><br></br>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</div>
+                </div>
+                <div className='col-sm-4 wrap'>
+                    <img class="card-img-top" src="https://previews-te.wetransfer.net/file/wetransfer/j1oc/sgsxddn0a3y7tg6mh20220729115838/sd13x28csqric9qon20220729115838?width=2048&height=2048&source=spaceship&origin=collect-free&s=a173efae02da1ad207c7ba46ee77556c88d62cf0&Expires=1659541909&Signature=ZvwmkrCVGW~o~M4JOJSgYJWtTEfXeBXwcI34lhH1qzEuy4OMHonkONClBTninpCZ13ZV1MPbK02J~EyJX0hVQ-4GWYAtwy4mDJ073oZzAnpzeQqPtYPIXgCtMjmdThSHVq2zXcsrmQPzR1CaaiIT-d6Ryt9dNhbpJvqL1AbLcjfUB~Jx1XhnWrLaiXBhJByfHa30M2FyUjj9pTflmDvNSbmss7AHLEykgyVsVGpNtqa-KVD5XBfJsm9meBZXjYu03l0RzzBHa2VVGely45EVovHZwvm51WZNcwn6jKtuG6lCILRuS83wSKaLXl5fqXXDdPygpKZ7wqr7Vo2mX1pxKQ__&Key-Pair-Id=APKAIRLQFERKGUWFG7GQ" alt="Card image cap"/>
+                    <div className='text display-6 text-center'>Linda<br></br><br></br>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</div>
+                </div>
+                </div>
+            </div>
+        </div>
+)
+}
